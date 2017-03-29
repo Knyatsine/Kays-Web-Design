faf396bf3e1269a8e043bb0641153097af05b7ac
Kudzai Nyatsine <kudzai.nyatsine@outlook.com>
2017-03-29 15:16:02 +0100

    modified code

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,20 @@
 
 $(document).ready(function() {
 
-$("h1.jumbo1").mouseenter(function() {
-	$("h1.jumbo1").fadeIn("slow").fadeOut("slow");
+$("h1.jumbo1").mouseleave(function() {
+	$("h1.jumbo1").slideDown("slow");
 });
 
-$("h1.jumbo1").mouseleave(function() {
-	$("h1.jumbo1").fadeOut("slow").fadeIn("slow");
+$("h1.jumbo1").mouseenter(function() {
+	$("h1.jumbo1").slideUp("slow");
 });
 
 $("p.jumbo").mouseenter(function() {
-	$("p.jumbo").slideToggle("slow");
+	$("p.jumbo").slideUp("slow");
 });
 
 $("p.jumbo").mouseleave(function() {
-	$("p.jumbo").slideToggle("slow");
+	$("p.jumbo").slideDown("slow");
 });
 
 $("p").mouseenter(function() {
